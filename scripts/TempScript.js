f8e059f4a46677660d6d0fd6437da2f43a822309
virenvish <87319718+virenvish@users.noreply.github.com>
2023-05-27 13:56:18 +0530

    Initial Upload
    Initial Upload

---
+++ scripts/TempScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('.test-cart').click(function () {
+        window.location.href = './cart.html'
+    });
+
+    //Show hide login/signup div
+    $('#btn-sign-up').click(function () {       
+        $('.signup-area').show();
+        $('.login-area').hide();      
+    });
+
+    $('#btn-login').click(function () {
+        $('.signup-area').hide();
+        $('.login-area').show();       
+    });
+});
